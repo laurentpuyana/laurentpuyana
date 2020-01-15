c3e562e5c6987c0773fe21dec8d6d40a1a691154
laurentpuyana <55293250+laurentpuyana@users.noreply.github.com>
2020-01-15 13:12:16 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -1,1 +1,14 @@
+var i;
+for (var i = 0; i < 8; i++) {
+  document.write("My Fith Loop: ")
+  var answer= Math.random(2, i);
+  document.write(answer + "<br />");
+}
 
+var ind = 0
+while(ind < 8)
+  {
+    var answer= Math.sqrt(ind);
+    document.write(answer + "<br />");
+    ind++;
+  }
