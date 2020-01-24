57386e0a47caa8e3e773b7733f0347a6cd92e2d5
laurentpuyana <55293250+laurentpuyana@users.noreply.github.com>
2020-01-24 13:28:07 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -15,10 +15,27 @@ while(ind < 8)
 function myLoop(){
 var start = document.getElementById("start").value;
 var end = document.getElementById("end").value;
-for(var i = start; i <= end; i++)
+  var jump = document.getElementById("jump").value;
+  
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  start = Number(start);
+  end = Number(end);
+  jump = Number(jump);
+  
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  
+  for(var i = start; i <= end; i+=jump)
   {
     document.write('<p>' + i + '</p>');
   }
 }
+
+
 
 
